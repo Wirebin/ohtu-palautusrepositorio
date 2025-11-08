c2ec3226e42ecbd94cbfebc593a19edca8c8de6d
Wirebin <107724483+Wirebin@users.noreply.github.com>
2025-11-09 01:21:11 +0200

    Add player information

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nationality = dict['nationality']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.games = dict['games']
+
     def __str__(self):
-        return self.name
+        return f"{self.name:23}{self.team:18}{self.goals} + {self.assists} = {self.goals+self.assists}"
